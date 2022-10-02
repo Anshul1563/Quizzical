47637be7ab0bff55436a779ce60f3b445d7e13b6
Anshul1563 <pesanshul@gmail.com>
2022-10-02 16:37:14 +0530

    'First'

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ export default function App() {
     
     return (
       <div>
-          <p className="text-4xl bg-slate-900 text-white text-center"> Testing Tailwind...</p>    
+              
       </div>
     )
   }
